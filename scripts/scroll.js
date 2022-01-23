ab4cddfbfc56150df6c6b5d2a746dc78e5928b19
VetalSh <jedvemya86@gmail.com>
2022-01-23 21:06:04 +0200

    day 2

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,24 +1,31 @@
-const links = document.querySelectorAll('.header-menu__item a');
+const scrollFunc = () => {
+  const links = document.querySelectorAll('.header-menu__item a');
+  const linkCharacteristics = document.querySelector('.card-details__link-characteristics');
 
-seamless.polyfill();
+  const newArray = [...links, linkCharacteristics];
 
-links.forEach((element) => {
-  element.addEventListener('click', (event) => {
-    event.preventDefault();
-    
-    const id = element.getAttribute('href').substring(1);
-    const section = document.getElementById(id);
-    if (section) {
-      seamless.elementScrollIntoView( section, {
-        behavior: 'smooth',
-        block: 'start'
-      });
-    } else {
-      seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-        behavior: "smooth",
-        block: "center",
-        inline: "center",
-      });
-    };
+  seamless.polyfill();
+
+  newArray.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      event.preventDefault();
+      
+      const id = element.getAttribute('href').substring(1);
+      const section = document.getElementById(id);
+      if (section) {
+        seamless.elementScrollIntoView( section, {
+          behavior: 'smooth',
+          block: 'start'
+        });
+      } else {
+        seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+          behavior: "smooth",
+          block: "center",
+          inline: "center",
+        });
+      };
+    });
   });
-});
+};
+
+scrollFunc();
