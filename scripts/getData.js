ac642d42d08594e1c1b09dd8bb26ff3991701905
VetalSh <jedvemya86@gmail.com>
2022-01-26 17:54:47 +0200

    Day 3

--- scripts/getData.js
+++ scripts/getData.js
@@ -1,5 +1,21 @@
 const getData = () => {
-  fetch('https://jsonplaceholder.typicode.com/todos');
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then((response) => {
+    if(response.ok) {
+      return response.json();
+    } else {
+      throw new Error('The data was received with an error!');
+    }
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error.message);
+  })
+  .finally(() => {
+    console.log('finally');
+  });
 };
 
 getData();
